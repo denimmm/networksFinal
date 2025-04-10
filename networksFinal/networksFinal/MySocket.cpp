4a7d91268df0fe3fbfdbd50fb21c24d650446753
denimmm <dmacdougall7987@conestogac.on.ca>
2025-04-10 08:29:31 -0400

    added some basic functions and constructor

--- networksFinal/networksFinal/MySocket.cpp
+++ networksFinal/networksFinal/MySocket.cpp
@@ -1,23 +1,54 @@
 #include "MySocket.h"
 
-MySocket::MySocket(SocketType, std::string, unsigned int, ConnectionType, unsigned int)
+//constructor
+MySocket::MySocket(SocketType stype, std::string newIP, unsigned int newport, ConnectionType ctype, unsigned int bufferSize)
 {
+    mySocket = stype;
+    IPAddr = newIP;
+    Port = newport;
+    connectionType = ctype;
+
+    if (bufferSize > 0) {
+        MaxSize = bufferSize;
+        Buffer = new char[bufferSize];
+    }
+    else {
+        MaxSize = DEFAULT_SIZE;
+        Buffer = new char[DEFAULT_SIZE];
+    }
+
+    //set up tcp or udp sockets to receive
+
+
 }
 
+//destructor
 MySocket::~MySocket()
 {
+    delete Buffer;
 }
 
+//establish 3-way handshake
 void MySocket::ConnectTCP()
 {
+    //dont make connection for UDP
+    if (connectionType == UDP) {
+        return;
+    }
+
+
+
 }
 
+//close the tcp
 void MySocket::DisconnectTCP()
 {
+
 }
 
 void MySocket::SendData(const char*, int)
 {
+
 }
 
 int MySocket::GetData(char*)
@@ -27,27 +58,30 @@ int MySocket::GetData(char*)
 
 std::string MySocket::GetIPAddr()
 {
-    
+    return IPAddr;
 }
 
-void MySocket::SetIPAddr(std::string)
+void MySocket::SetIPAddr(std::string newIP)
 {
+    IPAddr = newIP;
 }
 
-void MySocket::SetPort(int)
+void MySocket::SetPort(int newport)
 {
+    Port = newport;
 }
 
 int MySocket::GetPort()
 {
-    
+    return Port;
 }
 
 SocketType MySocket::GetType()
 {
-
+    return mySocket;
 }
 
-void MySocket::SetType(SocketType)
+void MySocket::SetType(SocketType type)
 {
+    mySocket = type;
 }
