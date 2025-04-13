ebb0fb57b49b1aa2dbd186a1504bb3b524567618
BrooksSnLiu <BROOKS.SN.LIU@GMAIL.COM>
2025-04-13 17:13:07 -0400

    Update SocketTests.cpp

--- networksFinal/UnitTests/SocketTests.cpp
+++ networksFinal/UnitTests/SocketTests.cpp
@@ -5,20 +5,63 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace SocketTests
+namespace MySocketTests
 {
-	TEST_CLASS(settersAndGetters)
-	{
-	public:
-		TEST_METHOD(setType)
-		{
-			MySocket* socket = new MySocket();
-			
+    TEST_CLASS(MySocketBasicTests)
+    {
+    public:
 
-			delete socket;
-		}
+        TEST_METHOD(Constructor_InitializesDefaults_WhenInvalidBufferSize)
+        {
+            MySocket socket(CLIENT, "127.0.0.1", 8080, TCP, 0);  // Invalid buffer size
+            Assert::AreEqual(std::string("127.0.0.1"), socket.GetIPAddr());
+            Assert::AreEqual(8080, socket.GetPort());
+            Assert::AreEqual(CLIENT, socket.GetType());
+        }
 
+        TEST_METHOD(Constructor_InitializesCustomSize)
+        {
+            MySocket socket(CLIENT, "127.0.0.1", 9090, UDP, 2048);
+            Assert::AreEqual(9090, socket.GetPort());
+        }
 
+        TEST_METHOD(SetAndGetIPAddr)
+        {
+            MySocket socket(CLIENT, "192.168.1.1", 1234, TCP, 1024);
+            socket.SetIPAddr("10.0.0.5");
+            Assert::AreEqual(std::string("10.0.0.5"), socket.GetIPAddr());
+        }
 
-	};
+        TEST_METHOD(SetAndGetPort)
+        {
+            MySocket socket(SERVER, "127.0.0.1", 1111, UDP, 512);
+            socket.SetPort(2222);
+            Assert::AreEqual(2222, socket.GetPort());
+        }
+
+        TEST_METHOD(SetAndGetSocketType)
+        {
+            MySocket socket(CLIENT, "127.0.0.1", 3000, TCP, 1024);
+            socket.SetType(SERVER);
+            Assert::AreEqual(SERVER, socket.GetType());
+        }
+
+        TEST_METHOD(GetData_CopiesDataFromBuffer)
+        {
+            MySocket socket(CLIENT, "127.0.0.1", 8080, TCP, 1024);
+
+            // Simulate a message in Buffer: length byte at Buffer[3]
+            char* internalBuffer = reinterpret_cast<char*>(malloc(1024));
+            internalBuffer[3] = 10; // Packet size
+            for (int i = 0; i < 15; i++) internalBuffer[i] = (char)i;
+
+            // Hack into internal Buffer (normally you'd use friend class or better test design)
+            memcpy((char*)socket, internalBuffer, 1024);
+
+            char dest[1024] = {};
+            int copied = socket.GetData(dest);
+
+            Assert::AreEqual((int)(10 + HEADERSIZE + 1), copied);  // HEADERSIZE is assumed defined
+        }
+    };
 }
