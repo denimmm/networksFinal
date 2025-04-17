6ebeaaee9fc9088b36646ef7525b884c7daeda0b
BrooksSnLiu <BROOKS.SN.LIU@GMAIL.COM>
2025-04-17 00:51:50 -0400

    Create script.js

---
+++ networksFinal/networksFinal/script.js
@@ -0,0 +1,29 @@
+const baseUrl = "http://localhost:5000";
+
+async function connect() {
+    const ip = document.getElementById("ip").value;
+    const port = document.getElementById("port").value;
+
+    const res = await fetch(`${baseUrl}/connect/${ip}/${port}`);
+    const text = await res.text();
+    document.getElementById("response").innerText = text;
+}
+
+async function sendTelecommand() {
+const cmdJson = document.getElementById("telecommand").value;
+const res = await fetch(`${baseUrl}/telecommand/`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: cmdJson
+    });
+const text = await res.text();
+document.getElementById("response").innerText = text;
+}
+
+async function getTelemetry() {
+const res = await fetch(`${baseUrl}/telemetry_request/`);
+const text = await res.text();
+document.getElementById("response").innerText = text;
+}
